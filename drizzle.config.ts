3b3f5c7422674b746ccf8975e1206cf7c58852c8
MISMOGPE\VABE <valentin.bertho@mismo.fr>
2026-03-11 14:56:44 +0100

    no message

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
-  schema: './lib/db/schema.ts',
-  out: './drizzle',
-  dialect: 'postgresql',
+  schema: "./lib/db/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.POSTGRES_URL!,
   },
